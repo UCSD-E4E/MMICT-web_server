6e7c337cd59feccda7a094e176340c696c04cca6
Leon Lin <lianglin.021@gmail.com>
2023-05-01 02:01:11 -0700

    added routes for frontend classification

--- src/controllers/classifyController.ts
+++ src/controllers/classifyController.ts
@@ -1,22 +1,37 @@
 import { Request, Response } from 'express';
 import { Image } from '../models/Image';
-import io from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const socket = io('ws://localhost:5000/ws-process');
-
-const classify = async (req: Request, res: Response) => {
+export const classify = async (req: Request, res: Response) => {
     try {
+
+        const socket = io('ws://127.0.0.1:5001/ws-process');
+
+        const dataType = req.body.dataType
+        const modelType = req.body.modelType
+
         const requestJson = {
             classifier_id: 0,
             processor_id: 0,
             image_ref: 'dummy-img-ref',
         };
+
+        console.log("classify");
         socket.emit('classify-parameters', JSON.stringify(requestJson));
+
+        socket.on('message', (data: string) => {
+          const result = JSON.parse(data);
+          console.log(`Received message: ${result}`);
+        });
+
+        socket.on('error', (data: string) => {
+          const result = JSON.parse(data);
+          console.log(`Received error: ${result}`);
+        });
+
+        // res.status(200).send("classify completed!");
+
     } catch (err) {
         res.status(500).json(err);
     }
-}
-
-module.exports = {
-    classify
 }
--- src/routes/classify.ts
+++ src/routes/classify.ts
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from "express";
 const router = express.Router();
-const uploadController = require('../controllers/uploadController');
+import { classify } from '../controllers/classifyController';
 
 router
   .route('/classify')
-  .post(uploadController.classify);
+  .post(classify);
 
-module.exports = router;
+export const classifyRouter = router;
