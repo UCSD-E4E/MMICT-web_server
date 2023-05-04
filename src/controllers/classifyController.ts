f0c458a41fe7d71a1593796a1dd161c6779c4366
devinmuzzy <68883649+devinmuzzy@users.noreply.github.com>
2023-05-04 12:27:29 -0700

    working websocket connection and return

--- src/controllers/classifyController.ts
+++ src/controllers/classifyController.ts
@@ -6,11 +6,11 @@ import { WebSocket } from 'ws'
 export const classify = async (req: Request, res: Response) => {
     try {
 
-      const PROCESSING_SERVICE_IP:string = 'ws://127.0.0.1:5000/ws-classify';
-      // const ECHO:string = 'ws://100.64.110.125:5001/echo'
+      const PROCESSING_SERVICE_IP:string = 'ws://100.64.214.98:5000/ws-process';
+      // const ECHO:string = 'ws://100.64.211.153:5001/echo'
 
       const ws:WebSocket = new WebSocket(PROCESSING_SERVICE_IP)
-
+      // res.setHeader("Content-type=")
       const requestJson = {
         classifier_id: 0,
         processor_id: 0,
@@ -21,16 +21,33 @@ export const classify = async (req: Request, res: Response) => {
       ws.on('error', (e:any) => console.log(e));
 
       ws.on('open', function open() {
-        console.log("HUH")
+        console.log("ws connection opened")
         ws.send(JSON.stringify(requestJson));
         console.log("sent %s", JSON.stringify(requestJson))
       });
       
-      ws.on('message', function message(data:any) {
-        console.log('received: %s', data);
-        res.status(269).send("kewl\n")
-      });
 
+      ws.on('message', (data:any) => ws_callback(data));
+    
+
+      function ws_callback(data:any){
+        console.log('msg: %s', data);
+        data = JSON.parse(String(data))
+        let data_status = data["status"]
+        console.log('status: %s', data_status)
+        if(data_status == 'DONE'){
+          if(!("geojson" in data)){
+            res.status(500)
+            res.statusMessage = "Geojson Not Returned"
+            res.send()
+          }
+          res.status(269)
+          res.statusMessage = "Groovy"
+          res.send(data["geojson"])
+        }
+      }
+      
+      // function message_callback(data:any){}
 
 
     } catch (err) {
