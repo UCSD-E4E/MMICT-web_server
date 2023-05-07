a613ef2093b1add12c03861ef72b9f7b609f0c8a
Leon Lin <lianglin.021@gmail.com>
2023-05-07 16:21:41 -0700

    fixed websocket connection with ip-service

--- src/controllers/classifyController.ts
+++ src/controllers/classifyController.ts
@@ -1,27 +1,29 @@
 import { Request, Response } from 'express';
 import { Image } from '../models/Image';
-import { io } from "socket.io-client";
 import { WebSocket } from 'ws'
 
 export const classify = async (req: Request, res: Response) => {
     try {
 
-      const PROCESSING_SERVICE_IP:string = 'ws://127.0.0.1:5000/ws-classify';
-      // const ECHO:string = 'ws://100.64.110.125:5001/echo'
+      console.log(`classification request: ${req.body.dataType}, ${req.body.modelType}`);
 
-      const ws:WebSocket = new WebSocket(PROCESSING_SERVICE_IP)
+      const WEBSERVER:string = "http://localhost:8000";
+      const PROCESSING_SERVICE_IP:string = 'ws://host.docker.internal:5001/ws-classify';
+
+      const ws:WebSocket = new WebSocket(PROCESSING_SERVICE_IP, {
+        origin: WEBSERVER,
+      })
 
       const requestJson = {
-        classifier_id: 0,
-        processor_id: 0,
+        data_type: req.body.dataType,
+        processor_id: req.body.modelType,
+        //todo: retrieve image reference from user db
         image_ref: 'dummy-img-ref',
-    };
-
+      };
 
       ws.on('error', (e:any) => console.log(e));
 
       ws.on('open', function open() {
-        console.log("HUH")
         ws.send(JSON.stringify(requestJson));
         console.log("sent %s", JSON.stringify(requestJson))
       });
@@ -30,7 +32,6 @@ export const classify = async (req: Request, res: Response) => {
         console.log('received: %s', data);
         res.status(269).send("kewl\n")
       });
-
 
 
     } catch (err) {
