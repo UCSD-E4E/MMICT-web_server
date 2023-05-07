36ae7a55160f9ffb0fc7e5fa3c956fec80b7bbaa
devinmuzzy <68883649+devinmuzzy@users.noreply.github.com>
2023-05-07 16:40:26 -0700

    made safer

--- src/controllers/classifyController.ts
+++ src/controllers/classifyController.ts
@@ -5,36 +5,47 @@ import { WebSocket } from 'ws'
 
 export const classify = async (req: Request, res: Response) => {
     try {
+      // command to run on commandline \/ \/ \/ 
+      // curl -X POST -H "Content-Type: application/json" -d '{"classifier_id": 0, "processor_id": 0, "image_ref":"test_ref"}' 127.0.0.1:8000/classify
 
-      const PROCESSING_SERVICE_IP:string = 'ws://100.64.214.98:5000/ws-process';
-      // const ECHO:string = 'ws://100.64.211.153:5001/echo'
-
+      // CHANGE IP TO THIS MACHINE'S IP !!
+      const PROCESSING_SERVICE_IP:string = 'ws://100.64.220.102:5000/ws-process';
       const ws:WebSocket = new WebSocket(PROCESSING_SERVICE_IP)
-      // res.setHeader("Content-type=")
-      const requestJson = {
-        classifier_id: 0,
-        processor_id: 0,
-        image_ref: 'dummy-img-ref',
-    };
 
+      console.log(req.body)
+      // Return a 400 error if anything is missing from the request body
+      if (!("classifier_id" in req.body && "processor_id" in req.body && "image_ref" in req.body)){
+        res.status(400)
+        res.statusMessage = "Missing Requirements"
+        res.send()
+        return
+      }
+
+      // fill in the request
+      let requestJson = {
+        classifier_id: req.body['classifier_id'],
+        processor_id: req.body['processor_id'],
+        image_ref: req.body['image_ref']
+      };
 
       ws.on('error', (e:any) => console.log(e));
+      ws.on('message', (data:any) => ws_callback(data));
 
+      // send request to ip_service
       ws.on('open', function open() {
         console.log("ws connection opened")
         ws.send(JSON.stringify(requestJson));
         console.log("sent %s", JSON.stringify(requestJson))
       });
-      
-
-      ws.on('message', (data:any) => ws_callback(data));
-    
-
+          
+      // callback function for recieving all incoming websocket communication
       function ws_callback(data:any){
         console.log('msg: %s', data);
+        // read incoming communication as a JSON object
         data = JSON.parse(String(data))
         let data_status = data["status"]
         console.log('status: %s', data_status)
+        // if classification is done, return geojson to the client if geojson exists
         if(data_status == 'DONE'){
           if(!("geojson" in data)){
             res.status(500)
@@ -43,15 +54,14 @@ export const classify = async (req: Request, res: Response) => {
           }
           res.status(269)
           res.statusMessage = "Groovy"
-          res.send(data["geojson"])
+          res.send(data)
         }
       }
       
-      // function message_callback(data:any){}
 
 
     } catch (err) {
-        res.status(200).send(err);
+        res.status(500).send(err);
         console.log(err)
     }
 }
