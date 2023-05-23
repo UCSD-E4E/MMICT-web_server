82a744bbda5b1f081424acf14656f607f45afae1
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2023-05-22 17:33:24 -0700

    finished route to fetch all images of user

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,11 +1,18 @@
-import { Request, Response } from 'express';
-import { Image } from '../models/Image';
+import { Request, Response, NextFunction } from 'express';
+import { NotFound } from '../error';
+import { User } from '../models/User';
 
-export const getImages = async (req: Request, res: Response) => {
+export const getImages = async (req: Request, res: Response, next: NextFunction) => {
+    const { username } = req.body;
+
     try {
-        const images = await Image.find({});
+        const user = await User.findOne({usernames: username});
+
+        if(user == null) {
+            return next(new NotFound('User with that username does not exist'));
+        }
     
-        res.status(200).json(images);
+        res.status(200).json(user.images);
     } catch (err) {
         res.status(500).json(err);
     }
