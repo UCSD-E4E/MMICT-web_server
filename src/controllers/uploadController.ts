be7128a19957f7cf9ee034ba9fdd8a66a8777299
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2023-12-06 17:22:05 -0800

    Update uploadController.ts
    In progress of implementing large file upload functionality

--- src/controllers/uploadController.ts
+++ src/controllers/uploadController.ts
@@ -1,19 +1,35 @@
-const AWS = require('aws-sdk');
 import { Request, Response, NextFunction } from 'express';
 const uuid = require('uuid').v4;
 import { User } from '../models/User';
 import { Image } from '../models/Image';
 
+// Imports for aws sdk
+import {
+    PutObjectCommand,
+    CreateMultipartUploadCommand,
+    UploadPartCommand,
+    CompleteMultipartUploadCommand,
+    AbortMultipartUploadCommand,
+    S3Client,
+  } from "@aws-sdk/client-s3";
+  
+// Set up
 require('dotenv').config();
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-const s3 = new AWS.S3({
+
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId,
+        secretAccessKey,
+    },
     region: process.env.AWS_REGION
 });
+
+// Obtaining file size
+const maxSingleFileSize = 100 * 1024 * 1024; //100MB
 
 export const uploadToS3 = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -22,6 +38,7 @@ export const uploadToS3 = async (req: Request, res: Response, next: NextFunction
         const files = req.files;
 
         files.forEach(async (file: any) => {
+
             const fileKey = uuid();
             let fileName = file.originalname.split(".");
             const fileType = fileName[fileName.length - 1];
@@ -31,12 +48,14 @@ export const uploadToS3 = async (req: Request, res: Response, next: NextFunction
                 Key: `${fileKey}.${fileType}`,
                 Body: file.buffer
             };
+            // Determine what method of upload we need
+            if (file.size > maxSingleFileSize) {
+                await uploadLargeFile(params, file);
+            }
+            else {
+                await uploadSmallFile(params);
+            }
 
-            s3.upload(params, (error: Error, data: any) => {
-                if(error) {
-                    throw error;
-                }
-            });
 
             const image = await Image.create({
                 name: fileName[0],
@@ -51,4 +70,31 @@ export const uploadToS3 = async (req: Request, res: Response, next: NextFunction
     } catch (err) {
         next(err);
     }
+}
+
+
+async function uploadLargeFile(params, file: any) {
+    const createString = (size = file.size) => {
+        return "x".repeat(size);
+    };
+
+    const main = async() => {
+        //todo
+    }
+}
+
+async function uploadSmallFile(params) {
+    const main = async () => {
+        const command = new PutObjectCommand({
+          Bucket: params.Bucket,
+          Key: params.Key,
+          Body: params.Body,
+        });
+        try {
+            const response = await s3.send(command);
+            console.log(response);
+          } catch (err) {
+            console.error(err);
+          }
+        };
 }
