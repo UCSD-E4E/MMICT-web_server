3d20ed6c86febb9e0ef21b496e3492ec19d7f5ea
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2024-01-23 01:46:20 -0800

    Update uploadController.ts
    finished code, need more testing

--- src/controllers/uploadController.ts
+++ src/controllers/uploadController.ts
@@ -11,15 +11,17 @@ import {
     CompleteMultipartUploadCommand,
     AbortMultipartUploadCommand,
     S3Client,
-  } from "@aws-sdk/client-s3";
+} from "@aws-sdk/client-s3";
   
 // Set up
 require('dotenv').config();
 
+
+// Extract AWS credentials from environment variables
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-
+// Create an S3 client with the extracted credentials and AWS region
 const s3 = new S3Client({
     credentials: {
         accessKeyId,
@@ -27,6 +29,7 @@ const s3 = new S3Client({
     },
     region: process.env.AWS_REGION
 });
+
 
 // Obtaining file size
 const maxSingleFileSize = 100 * 1024 * 1024; //100MB
@@ -73,17 +76,87 @@ export const uploadToS3 = async (req: Request, res: Response, next: NextFunction
 }
 
 
-async function uploadLargeFile(params, file: any) {
-    const createString = (size = file.size) => {
-        return "x".repeat(size);
-    };
+
+async function uploadLargeFile(params: any, file: any) {
+  
+    const minPartSize = 5 * 1024 * 1024;
+     
+    //Loop to find partSize
+    let numParts = 1;
+    while(Math.ceil(file.size / numParts) > minPartSize) {
+        ++numParts;
+    }
 
     const main = async() => {
         //todo
-    }
+        let uploadId;
+        try {
+            const multipartUpload = await s3.send(
+                new CreateMultipartUploadCommand({
+                    Bucket: params.Bucket,
+                    Key: params.Key,
+                }),
+            );
+
+            uploadId = multipartUpload.UploadId;
+
+            const uploadPromises = [];
+            // Multipart uplaods require a minimum size of 5 MB
+            const partSize = Math.ceil(file.size / numParts);
+            
+            // Upload each part
+            for (let i = 0; i < numParts; i++) {
+                const start = i * partSize;
+                const end = start + partSize;
+                uploadPromises.push(
+                    s3.send(
+                        new UploadPartCommand({
+                            Bucket: params.Bucket,
+                            Key: params.Key,
+                            UploadId: uploadId,
+                            Body: (params.Body).subarray(start,end),
+                            PartNumber: i + 1,
+                        }),
+                    )
+                    .then((d) => {
+                        console.log("Part", i+1, "uploaded");
+                        return d;
+                    }),
+                );
+            }
+            const uploadResults = await Promise.all(uploadPromises);
+
+            return await s3.send(
+                new CompleteMultipartUploadCommand({
+                    Bucket: params.Bucket,
+                    Key: params.Key,
+                    UploadId: uploadId,
+                    MultipartUpload: {
+                        Parts: uploadResults.map(({ ETag }, i) => ({
+                            ETag,
+                            PartNumber: i + 1,
+                        })),
+                    },
+                }),
+            );
+            // Verify output by downloading file
+        } catch (err) {
+            console.error(err);
+
+            if (uploadId) {
+                const abortCommand = new AbortMultipartUploadCommand ({
+                    Bucket: params.Bucket,
+                    Key: params.Key,
+                    UploadId: uploadId,
+                });
+
+                await s3.send(abortCommand);
+            }
+        }
+    };
 }
 
-async function uploadSmallFile(params) {
+async function uploadSmallFile(params: any) {
     const main = async () => {
         const command = new PutObjectCommand({
           Bucket: params.Bucket,
